f1bdc18aaf86f50d2380083f32d55d9b807728d8
Chris Angelico <rosuav@gmail.com>
2020-06-24 16:27:16 +1000

    Fix colon positioning in >1hr cooldowns

--- httpstatic/hypetrain.js
+++ httpstatic/hypetrain.js
@@ -35,7 +35,7 @@ function update() {
 		return;
 	}
 	let t = ":" + ("0" + (tm % 60)).slice(-2);
-	if (tm >= 3600) t = Math.floor(tm / 3600) + ("0" + (Math.floor(tm / 60) % 60)).slice(-2) + ":" + t;
+	if (tm >= 3600) t = Math.floor(tm / 3600) + ":" + ("0" + (Math.floor(tm / 60) % 60)).slice(-2) + t;
 	else t = Math.floor(tm / 60) + t; //Common case - less than an hour
 	time.innerHTML = t;
 }
