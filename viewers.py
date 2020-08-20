1ea10e5ef59c992bc4bcd942a2452ca8d0ee647c
Chris Angelico <rosuav@gmail.com>
2020-08-20 18:11:34 +1000

    viewers: Create some sort of outlier removal (optional)

--- viewers.py
+++ viewers.py
@@ -2,6 +2,7 @@ import json
 import sys
 import time
 import itertools
+import statistics
 import matplotlib.pyplot as plt
 
 if len(sys.argv) < 2:
@@ -14,8 +15,25 @@ stats = status["stream_stats"][sys.argv[1]]
 boundary = time.time() - 86400 * 62 # Take the past year. Or use 31 for a month, or 62 for two months, etc.
 boundary = 0 # Or show all available data
 max_graph_points = 100
+cull_outliers = False # Drop any data points that seem to be outliers (eg front page streams)
 
 stats = [s for s in stats if s["start"] >= boundary]
+if cull_outliers:
+	# For outlier detection, we just look at the high water mark.
+	data = [s["viewers_high"] for s in stats]
+	# TODO: Get a more statistically-reliable form of outlier detection
+	# This is basically "drop anything more than four standard deviations
+	# above the mean", except that we use the median, and if the stdev is
+	# greater than the median, we actually drop anything that's more than
+	# four medians above the median. It seems to give useful results.
+	avg = statistics.median(data)
+	basis = min(statistics.stdev(data), avg)
+	threshold = avg + 4 * basis
+	n = len(stats)
+	stats = [s for s in stats if s["viewers_high"] <= threshold]
+	dropped = n - len(stats)
+	if dropped: print("Discarded", dropped, "probable outliers.")
+
 if len(stats) > max_graph_points:
 	# Combine some stats together so there are no more than N points
 	factor = len(stats) // max_graph_points
@@ -29,6 +47,8 @@ if len(stats) > max_graph_points:
 			combined["viewers_high"] = max(combined["viewers_high"], cur["viewers_high"])
 		stats.append(combined)
 print(len(stats), "data points graphed.")
+print("From", time.ctime(stats[0]["start"]))
+print("To  ", time.ctime(stats[-1]["end"]))
 # for s in stats:
 #	print("%s %d-%d" % (time.ctime(s["start"]), s["viewers_low"], s["viewers_high"]))
 plt.plot([s["viewers_high"] for s in stats])
