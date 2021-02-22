3ba4ff52ce7664f796a180a5a3a16b93950e55ff
Chris Angelico <rosuav@gmail.com>
2021-02-22 17:50:09 +1100

    ws_sync: In theory, allow multiple websockets in independent groups

--- httpstatic/ws_sync.js
+++ httpstatic/ws_sync.js
@@ -4,28 +4,28 @@
 let handler = null;
 let send_socket; //If present, send() is functional.
 const protocol = window.location.protocol == "https:" ? "wss://" : "ws://";
-function connect()
+function connect(group)
 {
 	let socket = new WebSocket(protocol + window.location.host + "/ws");
 	socket.onopen = () => {
 		console.log("Socket connection established.");
-		socket.send(JSON.stringify({cmd: "init", type: ws_type, group: ws_group}));
+		socket.send(JSON.stringify({cmd: "init", type: ws_type, group}));
 		send_socket = socket; //Don't activate send() until we're initialized
 	};
 	socket.onclose = () => {
 		send_socket = null;
 		console.log("Socket connection lost.");
-		setTimeout(connect, 250);
+		setTimeout(connect, 250, group);
 	};
 	socket.onmessage = (ev) => {
 		let data = JSON.parse(ev.data);
 		console.log("Got message from server:", data);
 		if (!handler) return;
-		if (data.cmd === "update") handler.render(data);
+		if (data.cmd === "update") handler.render(data, group);
 	};
 }
 //When ready, import the handler code. It'll be eg "/subpoints.js" but with automatic mtime handling.
-async function init() {handler = await import(ws_code); connect();}
+async function init() {handler = await import(ws_code); connect(ws_group);}
 if (document.readyState !== "loading") init();
 else window.addEventListener("DOMContentLoaded", init);
 
