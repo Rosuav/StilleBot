07ddcb7610f3c63fcfe3f9013153c88c7b9903a1
Chris Angelico <rosuav@gmail.com>
2021-03-19 20:10:11 +1100

    ws_sync: Allow modules to specify a custom handler, possibly including a separate type

--- httpstatic/ws_sync.js
+++ httpstatic/ws_sync.js
@@ -1,15 +1,16 @@
 //Generic websocket synchronization handler
 //Relies on globals ws_type and ws_group
 
-let handler = null;
+let default_handler = null;
 let send_socket; //If present, send() is functional.
 const protocol = window.location.protocol == "https:" ? "wss://" : "ws://";
-export function connect(group)
+export function connect(group, handler)
 {
+	if (!handler) handler = default_handler;
 	let socket = new WebSocket(protocol + window.location.host + "/ws");
 	socket.onopen = () => {
 		console.log("Socket connection established.");
-		socket.send(JSON.stringify({cmd: "init", type: ws_type, group}));
+		socket.send(JSON.stringify({cmd: "init", type: handler.ws_type || ws_type, group}));
 		send_socket = socket; //Don't activate send() until we're initialized
 	};
 	socket.onclose = () => {
@@ -48,7 +49,7 @@ export function connect(group)
 	};
 }
 //When ready, import the handler code. It'll be eg "/subpoints.js" but with automatic mtime handling.
-async function init() {handler = await import(ws_code); connect(ws_group);}
+async function init() {default_handler = await import(ws_code); connect(ws_group);}
 if (document.readyState !== "loading") init();
 else window.addEventListener("DOMContentLoaded", init);
 
