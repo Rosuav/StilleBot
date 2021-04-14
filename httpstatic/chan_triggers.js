be5d3678947d4fab85b8d008bb9c4c8b98b15066
Chris Angelico <rosuav@gmail.com>
2021-04-14 17:57:40 +1000

    Drop a TODO and simplify the function away

--- httpstatic/chan_triggers.js
+++ httpstatic/chan_triggers.js
@@ -3,12 +3,7 @@ const {CODE, BR, TR, TD, SPAN, DIV, UL, LI, INPUT} = choc;
 import {render_item as render_command, add_hook, open_advanced_view} from "$$static||chan_commands.js$$";
 
 export const render_parent = DOM("#triggers tbody");
-export function render_item(msg) {
-	console.log(msg)
-	//TODO: Hide the "otherwise" and render it regardless. That way, tabular view
-	//still has value, although you can't change the trigger conditions.
-	return render_command(msg);
-}
+export const render_item = render_command;
 export function render_empty() {
 	render_parent.appendChild(TR([
 		TD({colSpan: 3}, "No triggers defined. Create one!"),
