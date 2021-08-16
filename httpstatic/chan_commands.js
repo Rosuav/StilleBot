90db85a153c97f59480105e1d22bdd3e3533abcc
Chris Angelico <rosuav@gmail.com>
2021-08-16 17:09:43 +1000

    Retain top-level flags when raw editing a command

--- httpstatic/chan_commands.js
+++ httpstatic/chan_commands.js
@@ -385,7 +385,7 @@ export function sockmsg_validated(data) {
 		DOM("#rawdlg").showModal();
 	}
 	else if (data.cmdname === "updateraw") {
-		DOM("#command_details").firstChild.replaceWith(render_command(data.response));
+		DOM("#command_details").firstChild.replaceWith(render_command(data.response, 1));
 		checkpos();
 		DOM("#rawdlg").close();
 	}
