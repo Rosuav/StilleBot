6c72ff6fe887ab538cec355a35a714e4b07f960d
Chris Angelico <rosuav@gmail.com>
2021-08-23 10:41:28 +1000

    Simplify imports a tad

--- httpstatic/chan_triggers.js
+++ httpstatic/chan_triggers.js
@@ -1,10 +1,9 @@
 import choc, {set_content, DOM, on} from "https://rosuav.github.io/shed/chocfactory.js";
 const {CODE, BR, TR, TD, SPAN, DIV, UL, LI, INPUT} = choc;
-import {render_item as render_command, add_hook, open_advanced_view, sockmsg_validated, sockmsg_loadfavs, favcheck} from "$$static||chan_commands.js$$";
+import {render_item, sockmsg_validated, sockmsg_loadfavs, favcheck} from "$$static||chan_commands.js$$";
+export {render_item, sockmsg_validated, sockmsg_loadfavs};
 
-export {sockmsg_validated, sockmsg_loadfavs};
 export const render_parent = DOM("#triggers tbody");
-export const render_item = render_command;
 export function render_empty() {
 	render_parent.appendChild(TR([
 		TD({colSpan: 3}, "No triggers defined. Create one!"),
