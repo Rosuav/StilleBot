bad70590000b10975cebf137b2ad9d730aab3d59
Chris Angelico <rosuav@gmail.com>
2022-01-21 21:07:02 +1100

    Display hype train prefs correctly (for now)

--- httpstatic/prefs.js
+++ httpstatic/prefs.js
@@ -1,5 +1,5 @@
 import choc, {set_content, DOM, on} from "https://rosuav.github.io/shed/chocfactory.js";
-const {SECTION, TEXTAREA, DIV, BR, UL, LI, LABEL, INPUT} = choc;
+const {A, BR, DIV, INPUT, LABEL, LI, SECTION, TEXTAREA, UL} = choc; //autoimport
 
 const render_pref = {
 	//Provide a function to render each thing, based on the prefs key
@@ -11,8 +11,13 @@ const render_pref = {
 		])))),
 	],
 
-	//TODO:
-	//hypetrain: val => ...
+	hypetrain: val => [
+		A({href: "/hypetrain"}, "Hype train tracker"),
+		UL([
+			LI("Alerts: " + ["start", "insistent", "ding"].map(k => val["use_" + k] ? val["vol_" + k] + "% " + k : "no " + k).join(", ")),
+			LI("Emotes: " + ["large", "checklist", "allrows"].filter(k => val["emotes_" + k]).join(", ")),
+		]),
+	],
 
 	//Any unknowns will be rendered with this
 	"": (key, val) => {
