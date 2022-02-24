81a96e534487e0892372c856232220682d249f83
Chris Angelico <rosuav@gmail.com>
2022-02-24 17:17:32 +1100

    Open up to multiple alert types (in principle)

--- httpstatic/alertbox.js
+++ httpstatic/alertbox.js
@@ -1,10 +1,11 @@
 import choc, {set_content, DOM, on} from "https://rosuav.github.io/shed/chocfactory.js";
-const {FIGCAPTION, FIGURE, IMG, P} = choc; //autoimport
+const {AUDIO, FIGCAPTION, FIGURE, IMG, P} = choc; //autoimport
 
 const alert_formats = {
 	text_under: data => FIGURE({className: "text_under"}, [
 		IMG({src: data.image}),
 		FIGCAPTION({"data-textformat": data.textformat}),
+		AUDIO({preload: "auto", src: data.sound, volume: data.volume}),
 	]),
 };
 
@@ -26,26 +27,30 @@ const alert_formats = {
 let alert_length = 6, alert_gap = 2;
 
 export function render(data) {
+	//If, in the future, I need more than one alert type (with distinct formats),
+	//replace <main></main> with a set of position-absolute tiles, all on top of
+	//each other, each with an ID that says what it is. Alert queueing would be
+	//shared across all of them, but each alert type would activate a different
+	//element. TODO: If one audio is playing, don't fire another.
 	if (data.alert_format) {
-		if (alert_formats[data.alert_format]) set_content("main", alert_formats[data.alert_format](data));
-		else set_content("main", P("Unrecognized alert format, check editor or refresh page"));
+		if (alert_formats[data.alert_format]) set_content("#hostalert", alert_formats[data.alert_format](data));
+		else set_content("#hostalert", P("Unrecognized alert format, check editor or refresh page"));
 	}
 	if (data.alert_length) alert_length = data.alert_length;
-	if (data.sound) DOM("#alertsound").src = data.sound;
-	if (typeof data.volume !== "undefined") DOM("#alertsound").volume = data.volume; //0 is not the same as absent
 }
 
-function remove_alert() {
-	DOM("main").classList.remove("active");
-	//If there's a queued alert, schedule it for another 1-2 seconds from now
+function remove_alert(alert) {
+	DOM(alert).classList.remove("active");
+	//If there's a queued alert, schedule it for another alert_gap seconds from now
 }
 
-function do_alert(channel, viewers) {
-	document.querySelectorAll("main [data-textformat]").forEach(el =>
+function do_alert(alert, channel, viewers) {
+	document.querySelectorAll(alert + " [data-textformat]").forEach(el =>
 		set_content(el, el.dataset.textformat.replace("{NAME}", channel).replace("{VIEWERS}", viewers))
 	);
-	DOM("main").classList.add("active");
-	DOM("#alertsound").play();
-	setTimeout(remove_alert, alert_length * 1000);
+	DOM(alert).classList.add("active");
+	DOM(alert + " audio").play();
+	//setTimeout(remove_alert, alert_length * 1000, alert);
 }
-window.ping = () => do_alert("Test", 42);
+window.ping = () => do_alert("#hostalert", "Test", 42);
+setTimeout(do_alert, 500, "#hostalert", "Demo", 123);
