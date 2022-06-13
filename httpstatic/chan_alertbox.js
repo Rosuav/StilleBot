03123494228816260d0f819c65a8886f3546195c
Chris Angelico <rosuav@gmail.com>
2022-06-14 07:15:33 +1000

    alertbox: Write down a TODO with a prerequisite

--- httpstatic/chan_alertbox.js
+++ httpstatic/chan_alertbox.js
@@ -422,6 +422,7 @@ export function render(data) {
 			update_visible_form();
 		}
 	}
+	//TODO: Migrate this to autorender, but first, have the alertidx come from the server
 	if (data.replay) {
 		const ofs = data.replay_offset || 0;
 		if (!data.replay.length) set_content("#replays", "No events to replay");
