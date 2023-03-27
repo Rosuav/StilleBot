9e303b3984ce71d0a77ea37e6502963accd89560
Chris Angelico <rosuav@gmail.com>
2023-03-27 11:40:46 +1100

    Build an editing front end

--- httpstatic/tradingcards.js
+++ httpstatic/tradingcards.js
@@ -1,15 +1,28 @@
 import {lindt, replace_content, DOM} from "https://rosuav.github.io/choc/factory.js";
-const {DIV, H1, H2, IMG, LI, SECTION, UL} = choc; //autoimport
+const {DIV, FIGCAPTION, FIGURE, H1, H2, IMG, INPUT, LI, SECTION, UL} = choc; //autoimport
 
-function TRADING_CARD(info) {
+let now_editing = null;
+
+function TRADING_CARD(info, editmode) {
+	if (editmode === 2) {
+		now_editing = info;
+		return [
+			FIGURE([FIGCAPTION("Preview"), TRADING_CARD(info)]),
+			FIGURE([FIGCAPTION("Edit"), TRADING_CARD(info, 1)]),
+		];
+	}
 	//TODO: Get the channel background colour to use as the card's colour
+	const EDIT = editmode ? (name, value) => INPUT({name, value}) : (n, v) => v;
 	return SECTION({class: "card"}, [
-		H1(info.card_name),
+		H1(EDIT("card_name", info.card_name)),
 		IMG({src: info.image}),
 		//Type line might need a rarity marker
-		H2(["Streamer — ", info.type]),
+		H2(["Streamer — ", EDIT("type", info.type)]),
 		DIV({class: "rules"}, [
-			UL(info.tags.map(t => LI(t))),
+			UL([
+				info.tags.map((t, i) => LI(EDIT("tags:" + i, t))),
+				editmode && LI(EDIT("tags:" + info.tags.length, "")),
+			]),
 		]),
 	]);
 }
@@ -20,9 +33,20 @@ on("submit", "#pickstrm", async e => {
 	if (streamer === "") return;
 	const info = await (await fetch("/tradingcards?query=" + encodeURIComponent(streamer))).json();
 	console.log(info);
-	replace_content("#build_a_card", TRADING_CARD(info.details));
+	replace_content("#build_a_card", TRADING_CARD(info.details, 2));
 });
 //hack
 fetch("/tradingcards?query=devicat").then(r => r.json())
-	.then(info => replace_content("#build_a_card", TRADING_CARD(info.details)));
+	.then(info => replace_content("#build_a_card", TRADING_CARD(info.details, 2)));
+
+on("change", "figure input", e => {
+	if (!now_editing) return;
+	const parts = e.match.name.split(":");
+	//If name is "type", set now_editing["type"] to the value. If it's
+	//"tag:5", set now_editing["tag"]["5"] to the value.
+	let dest = now_editing;
+	for (let i = 0; i < parts.length - 1; ++i) dest = dest[parts[i]];
+	dest[parts[parts.length - 1]] = e.match.value;
+	replace_content("#build_a_card", TRADING_CARD(now_editing, 2))
+});
 
