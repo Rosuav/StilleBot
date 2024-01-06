adcf6e0201c603069f3fde094a602a639f75266c
Chris Angelico <rosuav@gmail.com>
2024-01-07 10:21:14 +1100

    Show the substring matches too

--- httpstatic/regexp.js
+++ httpstatic/regexp.js
@@ -1,5 +1,5 @@
-import choc, {set_content, DOM} from "https://rosuav.github.io/choc/factory.js";
-const {SPAN} = choc;
+import {choc, set_content, DOM} from "https://rosuav.github.io/choc/factory.js";
+const {BR, CODE, SPAN} = choc; //autoimport
 
 function send() {ws_sync.send({cmd: "test", regexp: DOM("#regexp").value, text: DOM("#text").value});}
 on("input", "#regexp,#text", send);
@@ -7,6 +7,9 @@ send();
 
 export function sockmsg_testresult(msg) {
 	if (msg.error) set_content("#result", msg.error).className = "regex-error";
-	else if (msg.matches) set_content("#result", ["Matches ", SPAN({className: "regex-match"}, msg.matchtext[0])]).className = "";
+	else if (msg.matches) set_content("#result", msg.matchtext.map((m, i) =>
+		!i ? ["Matches ", SPAN({class: "regex-match"}, m)] //The main match
+		: [BR(), CODE("{regexp" + i + "}"), " - ", SPAN({class: "regex-match"}, m)] //Any subexpression matches
+	)).className = "";
 	else set_content("#result", "Doesn't match").className = "regex-nomatch";
 }
