2ec84f3b7bb8f26f92eb033a4127745a9a22b811
Chris Angelico <rosuav@gmail.com>
2024-05-24 21:39:06 +1000

    streamsetup: Make setups clickable

--- httpstatic/chan_streamsetup.js
+++ httpstatic/chan_streamsetup.js
@@ -2,9 +2,13 @@ import {choc, on} from "https://rosuav.github.io/choc/factory.js";
 const {BUTTON, TD, TR} = choc; //autoimport
 import {simpleconfirm} from "./utils.js";
 
+const setups = {}; //Keyed by ID
+
 export const render_parent = DOM("#setups tbody");
 export function render_item(msg, obj) {
-	return msg && TR({"data-id": msg.id}, [
+	if (!msg) return 0;
+	setups[msg.id] = msg;
+	return TR({"data-id": msg.id}, [
 		TD(msg.category), //Include box art?
 		TD(msg.title),
 		TD(msg.tags), //may need to be joined
@@ -20,14 +24,28 @@ export function render_empty() {
 }
 export function render(data) { }
 
+on("click", "#setups tr[data-id]", e => {
+	console.log("click");
+	const setup = setups[e.match.dataset.id];
+	if (!setup) return; //Shouldn't happen
+	const setupform = DOM("#setupconfig").elements;
+	setupform.category.value = setup.category;
+	setupform.title.value = setup.title;
+	setupform.tags.value = setup.tags;
+	setupform.ccls.value = setup.ccls;
+	setupform.comments.value = setup.comments;
+	DOM("#setupconfig").classList.add("dirty");
+});
+
+on("input", "#setupconfig input", e => e.match.form.classList.add("dirty"));
+
 on("submit", "#setupconfig", e => {
 	e.preventDefault();
-	//TODO: Update stream setup
+	//TODO: Update stream setup (and remove Dirty flag - only here, not when you save)
 });
 
 on("click", "#save", e => {
 	const el = DOM("#setupconfig").elements;
-	console.log(el);
 	const msg = {cmd: "newsetup"};
 	"category title tags ccls comments".split(" ").forEach(id => msg[id] = el[id].value);
 	ws_sync.send(msg);
