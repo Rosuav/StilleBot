c01a0f8de87d2696384a30d063db0997c2db044c
Chris Angelico <rosuav@gmail.com>
2024-06-14 21:53:38 +1000

    serverstatus: Differentiate Sikorsky and Gideon attributes

--- httpstatic/serverstatus.js
+++ httpstatic/serverstatus.js
@@ -1,14 +1,20 @@
 import {choc, set_content, DOM} from "https://rosuav.github.io/choc/factory.js";
 const {P, SPAN} = choc; //autoimport
 
-const attrs = {cpu: "CPU", gpu: "GPU", vram: "VRAM", enc: -1, dec: -1, spinner: -1};
+const sharedattrs = {cpu: "CPU", spinner: -1};
+const attrs = {
+	Gideon: {...sharedattrs},
+	Sikorsky: {...sharedattrs, gpu: "GPU", vram: "VRAM", enc: -1, dec: -1},
+};
 
-set_content("#content", ["Gideon", "Sikorsky"].map(id => P({id}, [
-	SPAN({class: "label"}, id + ": "),
-	Object.keys(attrs).map(name => attrs[name] !== -1 && [attrs[name] + ": ", SPAN({class: name + " percent"}), " "]),
-	"Enc: ", SPAN({class: "enc percent"}), ":", SPAN({class: "dec percent"}), " ",
-	SPAN({class: "spinner"}),
-])));
+set_content("#content", ["Gideon", "Sikorsky"].map(id => {
+	return P({id}, [
+		SPAN({class: "label"}, id + ": "),
+		Object.keys(attrs[id]).map(name => attrs[id][name] !== -1 && [attrs[id][name] + ": ", SPAN({class: name + " percent"}), " "]),
+		attrs[id].enc && ["Enc: ", SPAN({class: "enc percent"}), ":", SPAN({class: "dec percent"}), " "],
+		SPAN({class: "spinner"}),
+	]);
+}));
 function minify() {
 	const content = DOM("#content");
 	DOM("main").replaceWith(content, DOM("main style"));
@@ -18,14 +24,14 @@ if (location.hash === "#mini") minify();
 on("click", 'a[href="#mini"]', minify);
 
 function update(data, par) {
-	Object.keys(data).forEach(name => attrs[name] && set_content(par + " ." + name, ""+data[name]));
+	Object.keys(data).forEach(name => attrs[par][name] && set_content("#" + par + " ." + name, ""+data[name]));
 }
-export function render(data) {update(data, "#Sikorsky");}
+export function render(data) {update(data, "Sikorsky");}
 export const ws_host = "sikorsky.mustardmine.com";
 export const ws_config = {quiet: {msg: 1}};
 
 ws_sync.connect("", {
 	ws_config: {quiet: {msg: 1}},
 	ws_host: "gideon.mustardmine.com",
-	render: data => update(data, "#Gideon"),
+	render: data => update(data, "Gideon"),
 });
