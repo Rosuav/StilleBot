68a954b565fdfce2e4f52db9289b3687712c584d
Chris Angelico <rosuav@gmail.com>
2025-11-04 11:26:34 +1100

    Implement "Cute" RPS mode!

--- httpstatic/chan_minigames.js
+++ httpstatic/chan_minigames.js
@@ -17,7 +17,7 @@ export function render(data) {
 			"Theme ",
 			SELECT({name: "theme", value: rps.theme}, [
 				//OPTION({value: "default"}, "Default - rock/paper/scissors"), //Vanilla theme, may or may not happen
-				OPTION({value: "cute", disabled: true}, "Cute - rock/paper/scissors"), //DeviCat upcoming theme
+				OPTION({value: "cute"}, "Cute - rock/paper/scissors"), //DeviCat upcoming theme
 				OPTION({value: "halloween"}, "Halloween - knife/pumpkin/ghost"),
 			]),
 		]),
