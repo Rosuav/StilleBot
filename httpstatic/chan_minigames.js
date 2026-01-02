de78e65f6cd001455f7f81ccc5947b9cc4737898
Chris Angelico <rosuav@gmail.com>
2026-01-02 21:13:20 +1100

    minigames: Annotate the "drag to OBS" for clarity

--- httpstatic/chan_minigames.js
+++ httpstatic/chan_minigames.js
@@ -45,7 +45,7 @@ export function render(data) {
 			]),
 			" Initial size of all avatars; winners grow, losers disappear",
 		]),
-		rps.monitorid && LI(["To see the bar, ", A({class: "monitorlink", href: "monitors?view=" + rps.monitorid}, "drag this to OBS")]),
+		rps.monitorid && LI(["To see the bar, ", A({class: "monitorlink", href: "monitors?view=" + rps.monitorid}, "drag this to OBS"), " or add it as a browser source"]),
 		rps.monitorid && LI(["Further configuration (eg colour) can be done ", A({href: "monitors"}, "by editing the monitor itself"), "."]),
 		//TODO maybe: Channel point redemption for triggering a merge party
 	]));
@@ -104,7 +104,7 @@ export function render(data) {
 			//If the select isn't on "increase by...", hide the input :)
 			STYLE("select[name=hpgrowth]:not(:has( option[value=\"1\"]:checked )) ~ input[name=hpgrowth] {display: none;}"),
 		]),
-		boss.monitorid && LI(["To see the bar, ", A({class: "monitorlink", href: "monitors?view=" + boss.monitorid}, "drag this to OBS")]),
+		boss.monitorid && LI(["To see the bar, ", A({class: "monitorlink", href: "monitors?view=" + boss.monitorid}, "drag this to OBS"), " or add this as a browser source"]),
 		boss.monitorid && LI(["Further configuration (colour, font, etc) can be done ", A({href: "monitors"}, "by editing the bar itself"), "."]),
 		boss.monitorid && LI(["For testing purposes, you may ", BUTTON({id: "dealdamage"}, "deal some damage to the boss")]),
 	]));
