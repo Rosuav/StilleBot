a8339d2ca943675ec9e79bf9f2397ed8b25c234b
Chris Angelico <rosuav@gmail.com>
2026-06-12 21:57:21 +1000

    svgtest: Connect to the monitor (at least for styling - some things still cheated)

--- httpstatic/svgtest.js
+++ httpstatic/svgtest.js
@@ -1,9 +1,11 @@
 import {choc, set_content, DOM} from "https://rosuav.github.io/choc/factory.js";
 const {"svg:feBlend": FE_BLEND, "svg:feFlood": FE_FLOOD, "svg:filter": FILTER, "svg:rect": RECT, "svg:svg": SVG, "svg:text": TEXT} = choc; //autoimport
 import {ensure_font} from "$$static||utils.js$$";
+import {connect} from "$$static||ws_sync.js$$";
 
-function render(t) {
-	//Parsed out of the thresholds and rendered text
+function render(data) {
+	const t = data.data;
+	//CHEAT: These are parsed out of the thresholds and rendered text (see monitor.js)
 	const mark = 48, text = "Next bug tier: 21", goal_text = "500", pos_text = "240";
 	ensure_font(t.font);
 	ensure_font('"Noto Color Emoji"'); //Hack - ensure that emojis work
@@ -38,49 +40,7 @@ function render(t) {
 	]));
 }
 
-//Directly from the server
-render({
-	invertfill: 1,
-	"active": true,
-	"altcolor": "#000000",
-	"barcolor": "#9bfffd",
-	"bit": "1",
-	"bordercolor": "#000000",
-	"borderradius": "",
-	"color": "#000000",
-	"css": "",
-	"display": "20480:Next bug tier: #",
-	"fillcolor": "#34c02a",
-	"follow": "0",
-	"font": "\"Odibee Sans\"",
-	"fontsize": "32",
-	"fontstyle": "normal",
-	"fontweight": "normal",
-	"format": "plain",
-	"format_style": "2",
-	"fw_dono": "",
-	"fw_gift": "",
-	"fw_member": "",
-	"fw_shop": "",
-	"height": "40",
-	"id": "dLiYjuwOU4tHPbFh6lEcIazgXp7PBEoB409i",
-	"infinitier": true,
-	"kofi_commission": "",
-	"kofi_dono": "1",
-	"kofi_member": "",
-	"kofi_renew": "",
-	"kofi_shop": "1",
-	"lvlupcmd": "",
-	"needlesize": "0.375",
-	"previewbg": "#000fb6",
-	"sub_t1": "250",
-	"sub_t2": "500",
-	"sub_t3": "1250",
-	"text": "$bugs$:Next bug tier: #",
-	"text_css": "color: #000000;font-weight: normal;font-style: normal;border-color: #000000;font-size: 32px;width: 780px;height: 40px;border-radius: px;font-family: \"Odibee Sans\";padding-top: 0em; padding-bottom: 0em;padding-left: 0em; padding-right: 0em;--barcolor: #9bfffd;--fillcolor: #34c02a;--altcolor: #000000;",
-	"thresholds": "1000",
-	"thresholds_rendered": "1000",
-	"tip": "1",
-	"type": "goalbar",
-	"width": "780",
+connect("dLiYjuwOU4tHPbFh6lEcIazgXp7PBEoB409i#49497888", {
+	ws_type: "chan_monitors",
+	render,
 });
