270ee63cbbd04353baca881c10ce2497d1667926
Chris Angelico <rosuav@gmail.com>
2026-06-26 20:14:02 +1000

    goals: Support all goal types and give appropriate descriptions

--- httpstatic/chan_goals.js
+++ httpstatic/chan_goals.js
@@ -1,15 +1,24 @@
 import {choc, set_content, DOM, on} from "https://rosuav.github.io/choc/factory.js";
 const {LI, SPAN, UL} = choc; //autoimport
 
+//Describe the goal, in a way grammatically suited to "5 / 10 followers"
+const goaltypes = {
+	follower: "followers",
+	subscription: "sub points",
+	subscription_count: "subs",
+	new_subscription: "new sub points",
+	new_subscription_count: "new subs",
+	new_bit: "bits",
+	new_cheerer: "cheerers",
+};
+
 export const render_parent = DOM("#goals");
 export function render_item(goal) {
-	let type = "of something";
-	if (goal.type === "subscription") type = "subs";
-	//Bits goal came through with a blank type! Argh!
 	return LI({"data-id": goal.id}, [
 		goal.description || "Goal", " ",
 		goal.current_amount + "", " / ", goal.target_amount + "",
-		" ", type, " (id ", goal.id, ")"
+		" ", goaltypes[goal.type] || "of something",
+		" (id ", goal.id, ")",
 	]);
 }
 
